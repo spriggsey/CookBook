6055b0d0085e9a9ef548b1cf47c26277eb30b555
Jack Spriggs <jack@askewbrook.com>
2019-02-08 10:15:46 +0000

    added base files for compiling scss and testing

---
+++ tests/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CookBook Playground</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<header>
+    <div class="navbar">
+        <div class="_logo">
+            <img src="//placehold.it/300" alt="">
+        </div>
+        <ul class="__menu">
+            <li class="__menu-item"><a class="__menu-link" href="">Item</a></li>
+            <li class="__menu-item"><a class="__menu-link" href="">Item</a></li>
+            <li class="__menu-item"><a class="__menu-link" href="">Item</a></li>
+            <li class="__menu-item"><a class="__menu-link" href="">Item</a></li>
+        </ul>
+    </div>
+</header>
+<main>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
